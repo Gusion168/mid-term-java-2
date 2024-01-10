46c4f561a2f7a6e781a3eba4f5f0dd9850562e10
Gusion168 <youlimseavlong@gmail.com>
2024-01-10 08:32:59 +0700

    exercise2

---
+++ src/exercise2.java
@@ -0,0 +1,39 @@
+class Rectangle {
+    // Data members
+    protected double length;
+    protected double width;
+
+    // Constructor
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    // Methods
+    public double getArea() {
+        return length * width;
+    }
+
+    public double getPerimeter() {
+        return 2 * (length + width);
+    }
+}
+
+class Square extends Rectangle {
+    // Constructor for Square
+    public Square(double side) {
+        // Call the constructor of the base class (Rectangle)
+        super(side, side);
+    }
+}
+
+ class Main {
+    public static void main(String[] args) {
+        // Example usage
+        Square square = new Square(5.0);
+
+        // Accessing methods from Rectangle class
+        System.out.println("Area of the square: " + square.getArea());
+        System.out.println("Perimeter of the square: " + square.getPerimeter());
+    }
+}
